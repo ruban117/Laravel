229a451638584ada0a86923f095a5c4a834b34d9
Ruban <rubanpathak706@gmail.com>
2024-01-03 21:46:35 +0530

    Route To View

---
+++ Route-To-View/resources/views/test.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing</title>
+</head>
+<body>
+    {{-- <h1> Name:- {{ $name }}</h1>
+    <h1> City:- {{ $city }}</h1>
+    {!! $alert  !!} --}}
+
+    @foreach ($user as $id=>$v)
+        <p>Id:- {{ $id }} | Name:- {{ $v['name'] }} | Contact:- {{ $v['phone'] }} | City:- {{ $v['city'] }}</p>
+    @endforeach
+
+</body>
+</html>
---
+++ Route-To-View/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/test',function(){
+//     return view('test',['name'=>'Ruban','city'=>"Naihati",'alert'=>'<script>alert("Hello");</script>']);
+// });
+
+// Route::get('/test',function(){
+//     return view('test')->with('name','Ruban')->with('city','Naihati')->with('alert','<script>alert("Hello");</script>');
+// });
+
+// Route::get('/test',function(){
+//     return view('test')->withName('Ruban')->withCity('Naihati')->withAlert('<script>alert("Hello");</script>');
+// });
+
+Route::get('/test',function(){
+    $names=[
+        1=>['name'=>'Ruban','phone'=>'6289814242','city'=>'Naihati'],
+        2=>['name'=>'Soumita','phone'=>'9883760672','city'=>'Bhadreswar'],
+        3=>['name'=>'Altab','phone'=>'9883760677','city'=>'Chuchura'],
+        4=>['name'=>'Sovon','phone'=>'9883860672','city'=>'Tarakeswar'],
+    ];
+    return view('test',['user'=>$names]);
+});
