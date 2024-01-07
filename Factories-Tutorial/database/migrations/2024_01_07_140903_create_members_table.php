49e053509d9e21853362e3c0ff6fa12b84b1b3fc
Ruban <rubanpathak706@gmail.com>
2024-01-07 20:33:29 +0530

    Factory In Laravel To Insert Fake Datas

---
+++ Factories-Tutorial/database/migrations/2024_01_07_140903_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id(); // This creates an auto-incrementing primary key column
+            $table->string('name', 30);
+            $table->string('email', 50)->unique();
+            $table->string('phone'); // This creates a unique, non-primary key column
+            $table->string('address');
+            $table->string('password', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
